f71de9237dbe96cb1d6400a165555c4aa1edee5d
Mohammed Tausif Uddin Ansari <mohammed.tausifansari@gmail.com>
2021-10-12 05:58:58 +0000

    MongoDB Connected

--- ecommerce/app.js
+++ ecommerce/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 require('dotenv').config();
 
+//app
+const app = express();
 
+//database
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser:true
+}).then(() => console.log("MongoDB Database Connected"));
+
+//routes
 app.get("/", (req, res) => {
     res.send("Hello From Node Updated");
 });
